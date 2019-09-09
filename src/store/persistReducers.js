bffde988020334c672a8bee62e70f925655731f4
Alex Tavella <alextavella@outlook.com>
2019-09-09 14:24:27 -0300

    Config routes

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -1,6 +1,8 @@
 import { persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-community/async-storage';
 
+// AsyncStorage.clear();
+
 export default reducers => {
   return persistReducer(
     {
